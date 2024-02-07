0bba8238925e6dc30567ab605a24e0cadd22b205
danielgg <dgallegos137@gmail.com>
2024-02-06 22:51:56 -0500

    creación de la estructura del proyecto y las clases model

---
+++ src/test/java/com/moa/ecommerce/SpringEcommerceMoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceMoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
