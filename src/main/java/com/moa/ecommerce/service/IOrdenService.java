b0285fdde864e32fced53163c8c10d0a8c6cba2c
danielgg <dgallegos137@gmail.com>
2024-02-21 01:05:25 -0500

    metodo para generar el numero de la orden

--- src/main/java/com/moa/ecommerce/service/IOrdenService.java
+++ src/main/java/com/moa/ecommerce/service/IOrdenService.java
@@ -1,8 +1,11 @@
 package com.moa.ecommerce.service;
 
+import java.util.List;
+
 import com.moa.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden save(Orden orden);
 
 }
